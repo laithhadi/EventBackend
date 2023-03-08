5a688b12561774702e047fe7b5710c1f4c70c81e
Laith Hadi <laithoday5@yahoo.com>
2023-03-08 09:50:37 +0000

    LH: Max length for schema string now working

--- Models/EventSchema.js
+++ Models/EventSchema.js
@@ -10,26 +10,26 @@ const eventSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "Event name is required"],
-        max: [100, "Event name must not exceed more than 100 characters"]
+        maxLength: [100, "Event name must not exceed more than 100 characters"]
     },
     description: {
         type: String,
         required: [true, "Event description is required"],
-        max: [500, "Event description must not exceed more than 500 characters"]
+        maxLength: [500, "Event description must not exceed more than 500 characters"]
     },
     location: {
         type: String,
         required: [true, "Event location is required"],
-        max: [58, "Event location must not exceed more than 58 characters"]
+        maxLength: [58, "Event location must not exceed more than 58 characters"]
     },
     venue: {
         type: String,
         required: [true, "Event venue is required"],
-        max: [85, "Event venue must not exceed more than 85 characters"]
+        maxLength: [85, "Event venue must not exceed more than 85 characters"]
     },
     photoUrl: {
         type: String,
-        max: [500, "Event photo URL must not exceed more than 500 characters"]
+        maxLength: [500, "Event photo URL must not exceed more than 500 characters"]
     },
     startDate: {
         type: Date,
